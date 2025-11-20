46101f39835c6d38dc95ceeb008a5dc7e7444fb1
rcriss266 <rcriss266@gmail.com>
2025-11-20 12:12:38 -0600

    commit numero tres

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class BubbleSort {
 
     public static void bubbleSort(int[] arr) {
@@ -8,7 +11,7 @@ public class BubbleSort {
             intercambio = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    
+
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -21,13 +24,35 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {5, 2, 9, 1, 5, 6};
+        try {
+            // 1. Leer archivo (entrada.txt)
+            File file = new File("entrada.txt");
+            Scanner sc = new Scanner(file);
 
-        System.out.println("Antes :");
-        for (int num : arr) System.out.print(num + " ");
-        bubbleSort(arr);
+            // 2. Guardar los números del archivo en una lista
+            List<Integer> lista = new ArrayList<>();
+            while (sc.hasNextInt()) {
+                lista.add(sc.nextInt());
+            }
+            sc.close();
 
-        System.out.println("\nDespués:");
-        for (int num : arr) System.out.print(num + " ");
+            // 3. Convertir lista a arreglo
+            int[] arr = lista.stream().mapToInt(i -> i).toArray();
+
+            // Mostrar antes
+            System.out.println("Antes:");
+            for (int num : arr) System.out.print(num + " ");
+
+            // Ordenar
+            bubbleSort(arr);
+
+            // Mostrar después
+            System.out.println("\nDespués:");
+            for (int num : arr) System.out.print(num + " ");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontró el archivo.");
+        }
     }
 }
+
